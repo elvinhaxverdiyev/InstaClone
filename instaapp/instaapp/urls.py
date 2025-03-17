59aaa4ec779f37d024d8158f4956e429d5a77403
Elvin <elvinhaxverdiyev777@gamil.com>
2025-03-17 22:38:12 +0400

    fixed some bugs

--- instaapp/instaapp/urls.py
+++ instaapp/instaapp/urls.py
@@ -4,7 +4,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 
-# Swagger və Redoc üçün Schema View yaradılır
 schema_view = get_schema_view(
     openapi.Info(
         title="Comment API",
