de57759ce606453e48fd097e11b245b1513c66eb
Elvin <elvinhaxverdiyev777@gamil.com>
2025-06-25 17:59:19 +0400

    fixed some problems

--- instaapp/instaapp/settings.py
+++ instaapp/instaapp/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split()
+ALLOWED_HOSTS = ['51.21.196.223', 'localhost', '127.0.0.1']
 
 
 # Application definition
