b21909a35df360ebc9c3f871024ac81a9a401b7b
Elvin <elvinhaxverdiyev777@gamil.com>
2025-06-29 21:54:28 +0400

    solved some problems for comment model

--- instaapp/comments/models.py
+++ instaapp/comments/models.py
@@ -4,19 +4,8 @@ from profiles.models import Profile
 from likes.models import Like
 
 class Comment(models.Model):
-    """
-    Model representing comments on posts.
+    """Represents a comment made by a user on a post."""
 
-    **Fields:**
-    - `user`: The profile of the user who wrote the comment.
-    - `post`: The post on which the comment is made.
-    - `text`: The content of the comment (max 2200 characters).
-    - `created_at`: The timestamp when the comment was created.
-
-    **Related Names:**
-    - `comments` on `Profile`: Allows reverse lookup to get all comments by a user.
-    - `comments` on `Post`: Allows reverse lookup to get all comments on a post.
-    """
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="comments")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     text = models.CharField(max_length=2200)
